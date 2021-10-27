108ee309148c1675fe99fbbded8aa3b81f633285
I'M OWDVER <56756946+owdver@users.noreply.github.com>
2021-10-27 16:25:41 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -61,120 +61,97 @@ async def start(bot, update, cmd):
                         disable_webpage_preview=True
                     )
                     return
-                
+                try:
+                    file_uid = update.command[1]
+                    except IndexError:
+                        file_uid = False
                         
-
+                    if file_uid:
+                        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
+                        
+                    if (file_id or file_type) == None:
+                        return
+                    caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
                     
-               await update.reply_text("𝚂𝚘𝚛𝚛𝚢 𝙳𝚞𝚍𝚎, 𝚈𝚘𝚞 𝚊𝚛𝚎 𝙱𝚊𝚗𝚗𝚎𝚍")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="<b>നിങ്ങൾക്ക് മൂവീസ് വേണോ? എങ്കിൽ തായെ കാണുന്ന ഞങ്ങളുടെ മെയിൻ ചാനലിൽ ജോയിൻ ചെയ്യുക\nഎന്നിട്ട് ഗ്രൂപ്പിൽ പോയി വീണ്ടും മൂവിയിൽ ക്ലിക് ചെയ്ത് start കൊടുത്തു നോക്കൂ..!😁</b>",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="⭕️ 𝙹𝚘𝚒𝚗 𝙾𝚞𝚛 𝙼𝚊𝚒𝚗 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ⭕️", url=f"https://t.me/{UPDATE_CHANNEL}")]
-              ])
-            )
-            return
-        except Exception:
-            await update.reply_text(f"<b>This bot should be the admin on your update channel</b>\n\n<b>ഈ ചാനലിൽ  @{UPDATE_CHANNEL} ബോട്ടിനെ അഡ്മിൻ ആക്. എന്നിട്ട് /start കൊടുക്</b>")
-            return  
-    try:
-        file_uid = update.command[1]
-    except IndexError:
-        file_uid = False
-    
-    if file_uid:
-        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
-        
-        if (file_id or file_type) == None:
-            return
-        
-        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
-        
-        if file_type == "document":
-        
-            await bot.send_document(
-                chat_id=update.chat.id,
-                document = file_id,
-                caption = caption,
-                parse_mode="html",
-                reply_to_message_id=update.message_id,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton
-                                (
-                                    '⭕️ 𝙹𝚘𝚒𝚗 𝙾𝚞𝚛 𝙼𝚊𝚒𝚗 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ⭕️', url=f"{MT_CHANNEL}"
+                    if file_type == "document":
+                        
+                        
+                        await bot.send_document(
+                            chat_id=update.chat.id,
+                            document = file_id,
+                            caption = caption,
+                            parse_mode="html",
+                            reply_to_message_id=update.message_id,
+                            reply_markup=InlineKeyboardMarkup(
+                                [
+                                    [
+                                        InlineKeyboardButton(
+                                            '⭕️ 𝙹𝚘𝚒𝚗 𝙾𝚞𝚛 𝙼𝚊𝚒𝚗 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ⭕️', url=f"{MT_CHANNEL}"
+                                        )
+                                    ]
+                                ]
+                            )
+                        )
+                        elif file_type == "video":
+                            
+                            
+                            await update.bot.send_video(
+                                chat_id=update.chat.id,
+                                video = file_id,
+                                caption = caption,
+                                parse_mode="html",
+                                reply_markup=InlineKeyboardMarkup(
+                                    [
+                                        [
+                                            InlineKeyboardButton(
+                                                '👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url="https://t.me/Owdver_Bot"
+                                            )
+                                        ]
+                                    ]
                                 )
-                        ]
-                    ]
-                )
-            )
-
-        elif file_type == "video":
-        
-            await update.bot.send_video(
-                chat_id=update.chat.id,
-                video = file_id,
-                caption = caption,
-                parse_mode="html",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton
-                                (
-                                    '👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url="https://t.me/Owdver_Bot"
+                            )
+                            
+                        elif file_type == "audio":
+                            
+                            await update.bot.send_audio(
+                                chat_id=update.chat.id,
+                                audio = file_id,
+                                caption = caption,
+                                parse_mode="html",
+                                reply_markup=InlineKeyboardMarkup(
+                                    [
+                                        [
+                                            InlineKeyboardButton(
+                                                '👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url="https://t.me/Owdver_Bot"
+                                            )
+                                        ]
+                                    ]
                                 )
-                        ]
-                    ]
-                )
-            )
-            
-        elif file_type == "audio":
-        
-            await update.bot.send_audio(
-                chat_id=update.chat.id,
-                audio = file_id,
-                caption = caption,
-                parse_mode="html",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton
-                                (
-                                    '👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url="https://t.me/Owdver_Bot"
-                                )
-                        ]
-                    ]
-                )
-            )
-
-        else:
-            print(file_type)
-        
-        return
-
-    buttons = [[
-        InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/OB_FILTERBOT?startgroup=botstart')
-        ],[
-        InlineKeyboardButton('👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url=f't.me/OWDVER_BOT'),
-        InlineKeyboardButton('𝙲𝙷𝙰𝙽𝙽𝙴𝙻 📢', url=f't.me/OB_LINKS')
-    ],[
-        InlineKeyboardButton('🔧 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url=f't.me/OWDVER_BOT'),
-        InlineKeyboardButton('𝙷𝙴𝙻𝙿 ⚙️', callback_data="help")
-    ]]
-    
-    reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-                update.from_user.first_name),
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
+                            )
+                            
+                        else:
+                            print(file_type)
+                        return
+                    buttons = [[
+                        InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/OB_FILTERBOT?startgroup=botstart')
+                    ],[
+                        InlineKeyboardButton('👨🏻‍💻 𝙲𝚁𝙴𝙰𝚃𝙾𝚁', url=f't.me/OWDVER_BOT'),
+                        InlineKeyboardButton('𝙲𝙷𝙰𝙽𝙽𝙴𝙻 📢', url=f't.me/OB_LINKS')
+                    ],[
+                        InlineKeyboardButton('🔧 𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url=f't.me/OWDVER_BOT'),
+                        InlineKeyboardButton('𝙷𝙴𝙻𝙿 ⚙️', callback_data="help")
+                    ]]
+                    
+                    reply_markup = InlineKeyboardMarkup(buttons)
+                    
+                    await bot.send_message(
+                        chat_id=update.chat.id,
+                        text=Translation.START_TEXT.format(
+                            update.from_user.first_name),
+                        reply_markup=reply_markup,
+                        parse_mode="html",
+                        reply_to_message_id=update.message_id
+                    )
 
 
 @Client.on_message(filters.command(["help"]) & filters.private, group=1)
